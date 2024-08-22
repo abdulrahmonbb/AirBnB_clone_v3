aeece8f0f12dcfcc7fcb1c050517a0185f9484e5
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-22 11:45:39 +0100

    fixed pep8 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,11 @@
 Starts a flask web application
 """
 import os
-from flask import Flask, Blueprint, jsonify, make_response
-from api.v1.views import app_views
 from models import storage
 from flask_cors import CORS
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,6 +23,7 @@ def teardown_appcontext(code):
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+    app.run(
+        host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
         port=int(os.getenv('HBNB_API_PORT', '5000'))
     )
