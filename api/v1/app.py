c625efd9e2fffe169373ce6a866e55a05721b8eb
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-22 12:05:43 +0100

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,6 +21,10 @@ def teardown_appcontext(code):
     """
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(
