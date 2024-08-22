1487999d94fb6b13d07085199fe6538eae000de5
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-22 11:40:18 +0100

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Index.py to connect to API
+"""
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """
+    hbnbStatus
+    """
+    return jsonify({"status": "OK"})
+
+
+if __name__ == "__main__":
+    pass
